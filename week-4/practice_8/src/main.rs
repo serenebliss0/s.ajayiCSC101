56b61046bfc55714249ff7136c8bbbdf212d597e
serenebliss0 <semajayi1234@gmail.com>
2025-10-20 11:37:03 +0100

    Practice 8

---
+++ week-4/practice_8/src/main.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+
+    let mut x =0;
+    loop 
+    {
+        x += 1;
+        println!("x is {}", x);
+
+        if x == 5 {
+            break;
+        }
+    }
+}
---
+++ week-4/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\Semire\OneDrive - Caleb British International School\Documents\School Documents (University)\COS101- Intro to Computing Sciences\s.ajayiCSC101\week-4\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\Semire\OneDrive - Caleb British International School\Documents\School Documents (University)\COS101- Intro to Computing Sciences\s.ajayiCSC101\week-4\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
