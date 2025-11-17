11bc0ee922679ec349300b0cbe40ced84fdfe619
serenebliss0 <semajayi1234@gmail.com>
2025-11-17 12:09:29 +0100

    Practice 1

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,13 @@
+
+fn main()
+{
+    let v:Vec<i64> = Vec::new();
+
+    //print size of vector
+    println!("\nThe length of the vector is: {}", v.len());
+
+    let v = vec!["Grace", "Effiong", "Basil", "Kareem", "Susan"];
+
+    println!("The length of the vector is {}", v.len());
+    
+}
