e338619b76c767a1ad2635677ce17c5f81b9c3df
serenebliss0 <semajayi1234@gmail.com>
2025-11-24 12:20:44 +0100

    Week 9 Exercises

--- week-9/project_2/src/main.rs
+++ week-9/project_2/src/main.rs
@@ -1,3 +1,70 @@
+use std::io::*;
+use std::fs::*;
+
 fn main() {
-    println!("Hello, world!");
+    struct Student {
+        name: String,
+        matric_number: String,
+        department: String,
+        level: String,
+    }
+
+    let mut students: Vec<Student> = Vec::new();
+
+    println!("How many students data do you want to enter?");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    let number_of_students: u32 = input.trim().parse().expect("Enter a positive whole number");
+
+    for i in 0..number_of_students {
+        println!("Enter name of student {}", i + 1);
+        let mut name_input = String::new();
+        std::io::stdin().read_line(&mut name_input).expect("Failed to read line");
+        let name = name_input.trim().to_string();
+
+        println!("Enter matric number of student {}", i + 1);
+        let mut matric_input = String::new();
+        std::io::stdin().read_line(&mut matric_input).expect("Failed to read line");
+        let matric_number = matric_input.trim().to_string();
+
+        println!("Enter department of student {}", i + 1);
+        let mut dept_input = String::new();
+        std::io::stdin().read_line(&mut dept_input).expect("Failed to read line");
+        let department = dept_input.trim().to_string();
+
+        println!("Enter level of student {}", i + 1);
+        let mut level_input = String::new();
+        std::io::stdin().read_line(&mut level_input).expect("Failed to read line");
+        let level = level_input.trim().to_string();
+
+        // create the struct
+        let student = Student {
+            name,
+            matric_number,
+            department,
+            level,
+        };
+
+        // push into the vector
+        students.push(student);
+
+        println!("Student {} added successfully!", i + 1);
+    }
+
+    // print all students
+    for (i, s) in students.iter().enumerate() 
+    {
+        println!("Student {}: {} | {} | {} | {} level", i + 1, s.name, s.matric_number, s.department, s.level);
+    }
+
+    let mut file = OpenOptions::new().append(true).create(true).open("students.txt").expect("Failed to write to file");
+
+    for s in &students 
+    {
+        let line = format!("{}\t{}\t{}\t{}\n", s.name, s.matric_number, s.department, s.level);
+        file.write_all(line.as_bytes()).expect("Failed to write to file");
+    }
+
+    println!("All students have been added to students.txt!");
+
 }
--- week-9/project_3/src/main.rs
+++ week-9/project_3/src/main.rs
@@ -1,3 +1,63 @@
+use std::io::*;
+use std::fs::*;
+
 fn main() {
-    println!("Hello, world!");
+    struct Commissioner {
+        name: String,
+        ministry: String,
+        geopolitical_zone: String,
+    }
+
+    let mut Commissioners: Vec<Commissioner> = Vec::new();
+
+    println!("How many commissioners are you working with?");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    let number_of_commissioners: u32 = input.trim().parse().expect("Enter a positive whole number");
+
+    for i in 0..number_of_commissioners {
+        println!("Enter name of commissioner {}", i + 1);
+        let mut name_input = String::new();
+        std::io::stdin().read_line(&mut name_input).expect("Failed to read line");
+        let name = name_input.trim().to_string();
+
+        println!("Enter the ministry the commissioner belongs to {}", i + 1);
+        let mut ministry_input = String::new();
+        std::io::stdin().read_line(&mut ministry_input).expect("Failed to read line");
+        let ministry = ministry_input.trim().to_string();
+
+        println!("Enter the geopolitical zone {}", i + 1);
+        let mut geopolitical_zone_input = String::new();
+        std::io::stdin().read_line(&mut geopolitical_zone_input).expect("Failed to read line");
+        let geopolitical_zone = geopolitical_zone_input.trim().to_string();
+
+        // create the struct
+        let commissioner = Commissioner {
+            name,
+            ministry,
+            geopolitical_zone
+        };
+
+        // push into the vector
+        Commissioners.push(commissioner);
+
+        println!("Commissioner {} added successfully!", i + 1);
+    }
+
+    // print all  commissioners
+    for (i, s) in Commissioners.iter().enumerate() 
+    {
+        println!("Commissioner {}: {} | {} | {}", i + 1, s.name, s.ministry, s.geopolitical_zone);
+    }
+
+    let mut file = OpenOptions::new().append(true).create(true).open("commissioners.txt").expect("Failed to write to file");
+
+    for s in &Commissioners 
+    {
+        let line = format!("{}\t{}\t{}\n", s.name, s.ministry, s.geopolitical_zone);
+        file.write_all(line.as_bytes()).expect("Failed to write to file");
+    }
+
+    println!("All commissioners have been added to commissioners.txt successfully");
+
 }
